9872cae7066cf8087a0b6ec598aa6795192d1971
Johann Horvat <johann@indiecam.com>
2018-01-14 18:46:52 +0100

    added code to probe function to detect fxos8700

--- fxos8700.c
+++ fxos8700.c
@@ -9,7 +9,71 @@
 #include <linux/module.h>
 #include <asm/uaccess.h>
 
-/*  function prototypes */
+/**
+ * defines
+ */
+#define FXOS8700_ADDRESS	(0x1f)
+#define FXOS8700_ID				(0xc7)
+
+#define ACCEL_MG_LSB_2G 	(0.000244F)
+#define ACCEL_MG_LSB_4G 	(0.000488F)
+#define ACCEL_MG_LSB_8G 	(0.000976F)
+
+#define ACCEL_RANGE				ACCEL_RANGE_4G
+
+/**
+ * typedefs
+ */
+typedef enum{																	// DEFAULT    TYPE
+	FXOS8700_REGISTER_STATUS          = 0x00,
+	FXOS8700_REGISTER_OUT_X_MSB       = 0x01,
+	FXOS8700_REGISTER_OUT_X_LSB       = 0x02,
+	FXOS8700_REGISTER_OUT_Y_MSB       = 0x03,
+	FXOS8700_REGISTER_OUT_Y_LSB       = 0x04,
+	FXOS8700_REGISTER_OUT_Z_MSB       = 0x05,
+	FXOS8700_REGISTER_OUT_Z_LSB       = 0x06,
+	FXOS8700_REGISTER_WHO_AM_I        = 0x0D,   // 11000111   r
+	FXOS8700_REGISTER_XYZ_DATA_CFG    = 0x0E,
+	FXOS8700_REGISTER_CTRL_REG1       = 0x2A,   // 00000000   r/w
+	FXOS8700_REGISTER_CTRL_REG2       = 0x2B,   // 00000000   r/w
+	FXOS8700_REGISTER_CTRL_REG3       = 0x2C,   // 00000000   r/w
+	FXOS8700_REGISTER_CTRL_REG4       = 0x2D,   // 00000000   r/w
+	FXOS8700_REGISTER_CTRL_REG5       = 0x2E,   // 00000000   r/w
+	FXOS8700_REGISTER_MSTATUS         = 0x32,
+	FXOS8700_REGISTER_MOUT_X_MSB      = 0x33,
+	FXOS8700_REGISTER_MOUT_X_LSB      = 0x34,
+	FXOS8700_REGISTER_MOUT_Y_MSB      = 0x35,
+	FXOS8700_REGISTER_MOUT_Y_LSB      = 0x36,
+	FXOS8700_REGISTER_MOUT_Z_MSB      = 0x37,
+	FXOS8700_REGISTER_MOUT_Z_LSB      = 0x38,
+	FXOS8700_REGISTER_MCTRL_REG1      = 0x5B,   // 00000000   r/w
+	FXOS8700_REGISTER_MCTRL_REG2      = 0x5C,   // 00000000   r/w
+	FXOS8700_REGISTER_MCTRL_REG3      = 0x5D,   // 00000000   r/w
+} fxos8700Registers_t;
+
+typedef struct fxos8700RawData_s{
+	/*! Value for x-axis */
+  int16_t x;
+	/*! Value for y-axis */
+  int16_t y;
+	/*! Value for z-axis */
+  int16_t z;
+} fxos8700RawData_t;
+
+typedef enum
+{
+	/*! ±0.244 mg/LSB */
+	ACCEL_RANGE_2G                    = 0x00,
+	/*! ±0.488 mg/LSB */
+	ACCEL_RANGE_4G                    = 0x01,
+	/*! ±0.976 mg/LSB */
+	ACCEL_RANGE_8G                    = 0x02
+} fxos8700AccelRange_t;
+
+
+/**
+ * function prototypes 
+ */
 static ssize_t driver_write(struct file* filep, const char __user* user, 
 			    size_t count, loff_t* offset);
 static ssize_t driver_read(struct file* filep, char __user* user, size_t count,
@@ -54,7 +118,7 @@ static struct i2c_driver fxos8700_driver={
 MODULE_DEVICE_TABLE(i2c, fxos8700_idtable);
 
 static struct i2c_board_info info_fxos8700={
-	I2C_BOARD_INFO("fxos8700", 0x1f), 
+	I2C_BOARD_INFO("fxos8700", FXOS8700_ADDRESS), 
 };
 
 static ssize_t driver_write(struct file* filep, const char __user* user, 
@@ -76,8 +140,10 @@ static ssize_t driver_write(struct file* filep, const char __user* user,
 
 static ssize_t driver_read(struct file* filep, char __user* user, size_t count,
 			   loff_t* offset){
-	unsigned long not_copied, to_copy;
-	char value, command;
+	unsigned long not_copied=0UL;
+	unsigned long to_copy=0UL;
+	char value=0x00;
+	char command=0x00;
 
 	command=0x01; // input port 1
 	i2c_master_send(slave, &command, 1);
@@ -91,18 +157,29 @@ static ssize_t driver_read(struct file* filep, char __user* user, size_t count,
 static int fxos8700_probe(struct i2c_client* client, 
 			  const struct i2c_device_id* id){
 	char buf[2];
+	char command=0x00;
+	char value=0x00;
 
 	printk("fxos8700_probe: %p %p \"%s\"- ", client, id, id->name);
 	printk("slaveaddr: %d, name: %s\n", client->addr, client->name);
-	if(client->addr !=0x20){
+	if(client->addr !=FXOS8700_ADDRESS){
 		printk("i2c_probe: not found\n");
 		return -1;
 	}
 	slave=client;
-	// configuration
-	buf[0]=0x06; // direction port 0
-	buf[1]=0x00; // output
-	i2c_master_send(client, buf, 2);
+	/* try to find/detect device */
+	command=FXOS8700_REGISTER_WHO_AM_I;
+	i2c_master_send(slave, &command, 1);
+	i2c_master_recv(slave, &value, 1);
+
+	if(value!=FXOS8700_ID){
+		printk("i2c_probe: FXOS8700 not found\n");
+		return -1;
+	}
+
+//	buf[0]=0x06; // direction port 0
+//	buf[1]=0x00; // output
+//	i2c_master_send(client, buf, 2);
 	return 0;
 }
 
